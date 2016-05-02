cc8e2f1a0c635b8646cc28402ae0cb42c6e03d08
f100cleveland <dustin.rinne@gmail.com>
2016-05-02 12:38:08 -0500

    Change kernel tc back to 4.9-kernel for now.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CONFIG := saber_defconfig
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-7.0-kernel/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
 
 # Optimizations
 STRICT_ALIASING := false
